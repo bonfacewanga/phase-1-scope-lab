a398986be61d9a4763e429c4e06bb5888e6db8e5
Bonface <bonniewanga34@gmail.com>
2023-07-07 00:59:30 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer=value;
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase();
+
+    return customerName;
+}
+
+function setBestCustomer() {
+
+    bestCustomer = "not bob";
+console.log(bestCustomer)
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = "maybe bob";
+    
+    return bestCustomer;
+
+}
+
+function changeLeastFavoriteCustomer() {
+    throw "Assignment to constant variable."
+}
